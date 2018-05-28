1337aa2c1bc28163e8ec83c77f74b6a48d5fdb2d
valejs <dajezotti@libero.it>
2018-05-28 18:33:26 +0200

    Revert "Update gatti-in-fila.js"
    This reverts commit f689d2f953967113f4b1f5d7014821e335763a7a.

--- esercizi-stringhe-numeri/gatti-in-fila.js
+++ esercizi-stringhe-numeri/gatti-in-fila.js
@@ -12,5 +12,3 @@
   Hint: Se vuoi usare l'arrotondamento ti consiglio di non usare Math.round (arrotonda all'intero superiore solo da .5 in su) ma utilizza Math.ceil (arrotonda sempre all'intero superiore).
   http://www.imparareaprogrammare.it
 */
-
-var cats =44; 
